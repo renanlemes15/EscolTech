d6a8da282cd10b4a763ba85e8fede103284ac563
Kauã Hilgenberg <kauahilgenbergg@gmail.com>
2025-09-23 23:02:37 -0300

    att

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GerenciamentoAlerta from "./pages/gerenciamentoAlerta";
 import MonitoramentoMapa from "./pages/monitoramento";
 
 function App() {
-  return <MonitoramentoMapa />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MonitoramentoMapa />} />
+        <Route path="/gerenciamento-alerta" element={<GerenciamentoAlerta />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
